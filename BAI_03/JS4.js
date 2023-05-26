6f88fd216de5f9b004f00b1fd5580ab3edc5413e
AnhhHoanggg <132649189+AnhhHoanggg@users.noreply.github.com>
2023-05-26 07:37:49 +0700

    Update JS4.js

--- BAI_03/JS4.js
+++ BAI_03/JS4.js
@@ -1,1 +1,19 @@
+<!DOCTYPE html> 
+<html> 
+<body> 
+  <p id="doanVan">Đây là một đoạn văn.</p> 
+  <button onclick="chuyenDoiHienThi()">Chuyển đổi hiển thị</button> 
+ 
+  <script>     
+    function chuyenDoiHienThi() {       
+        var doanVan = document.getElementById("doanVan"); 
+        if (doanVan.style.display === "none") {
+            doanVan.style.display = "block";
+        } else {
+            doanVan.style.display = "none";
+        }
 
+    } 
+  </script> 
+</body> 
+</html>
